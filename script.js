69325befa98d03fd06c5b426785ad0b4dd0de838
lazsnail <larrybain01@gmail.com>
2023-07-03 01:56:05 +1000

    Added more scales with help from ChatGPT

--- script.js
+++ script.js
@@ -1,16 +1,73 @@
-const keys = {
+keys = {
     "Ab Major": ['G#/Ab', 'A#/Bb', 'C', 'C#/Db', 'D#/Eb', 'F', 'G'],
+    "Ab Minor": ['G#/Ab', 'A#/Bb', 'B', 'C#/Db', 'D#/Eb', 'E', 'F#'],
     "A Major": ['A', 'B', 'C#/Db', 'D', 'E', 'F#/Gb', 'G#/Ab'],
+    "A Minor": ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
     "Bb Major": ['A#/Bb', 'C', 'D', 'Eb', 'F', 'G', 'A'],
+    "Bb Minor": ['A#/Bb', 'C', 'C#/Db', 'Eb', 'F', 'F#/Gb', 'G#/Ab'],
     "B Major": ['B', 'C#/Db', 'D#/Eb', 'E', 'F#/Gb', 'G#/Ab', 'A#/Bb'],
+    "B Minor": ['B', 'C#/Db', 'D', 'E', 'F#/Gb', 'G', 'A'],
     "C Major": ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
+    "C Minor": ['C', 'D', 'D#/Eb', 'F', 'G', 'G#/Ab', 'A#/Bb'],
     "C#/Db Major": ['C#/Db', 'D#/Eb', 'F', 'G#/Ab', 'A#/Bb', 'C'],
+    "C#/Db Minor": ['C#/Db', 'D#/Eb', 'E', 'F#/Gb', 'G#/Ab', 'A', 'B'],
     "D Major": ['D', 'E', 'F#/Gb', 'G', 'A', 'B', 'C#/Db', 'D'],
+    "D Minor": ['D', 'E', 'F', 'G', 'A', 'A#/Bb', 'C'],
     "E Major": ['E', 'F#/Gb', 'G#/Ab', 'A', 'B', 'C#/Db', 'D#/Eb'],
+    "E Minor": ['E', 'F#/Gb', 'G', 'A', 'B', 'C', 'D'],
     "Eb Major": ['D#/Eb', 'F', 'G', 'G#/Ab', 'A#/Bb', 'C', 'D'],
+    "Eb Minor": ['D#/Eb', 'F', 'F#/Gb', 'G#/Ab', 'A#/Bb', 'B', 'C#'],
     "F Major": ['F', 'G', 'A', 'A#/Bb', 'C', 'D', 'E'],
-    "F#/Gb major": ['F#/Gb', 'G#/Ab', 'A#/Bb', 'B', 'C#/Db', 'D#/Eb', 'F'],
+    "F Minor": ['F', 'G', 'G#/Ab', 'A#/Bb', 'C', 'C#/Db', 'D#/Eb'],
+    "F#/Gb Major": ['F#/Gb', 'G#/Ab', 'A#/Bb', 'B', 'C#/Db', 'D#/Eb', 'F'],
+    "F#/Gb Minor": ['F#/Gb', 'G#/Ab', 'A', 'B', 'C#/Db', 'D', 'E'],
     "G Major": ['G', 'A', 'B', 'C', 'D', 'E', 'F#/Gb'],
+    "G Minor": ['G', 'A', 'A#/Bb', 'C', 'D', 'D#/Eb', 'F'],
+    "Ab Harmonic Minor": ['G#/Ab', 'A#/Bb', 'C', 'C#/Db', 'D#/Eb', 'F', 'G'],
+    "A Harmonic Minor": ['A', 'B', 'C', 'D', 'E', 'F', 'G#/Ab'],
+    "Bb Harmonic Minor": ['A#/Bb', 'C', 'C#/Db', 'Eb', 'F', 'F#/Gb', 'A'],
+    "B Harmonic Minor": ['B', 'C#/Db', 'D', 'E', 'F#/Gb', 'G', 'A#/Bb'],
+    "C Harmonic Minor": ['C', 'D', 'D#/Eb', 'F', 'G', 'G#/Ab', 'C'],
+    "C#/Db Harmonic Minor": ['C#/Db', 'D#/Eb', 'E', 'F#/Gb', 'G#/Ab', 'A#/Bb', 'C#/Db'],
+    "D Harmonic Minor": ['D', 'E', 'F', 'G', 'A', 'A#/Bb', 'D'],
+    "Eb Harmonic Minor": ['D#/Eb', 'F', 'F#/Gb', 'G#/Ab', 'A#/Bb', 'B', 'D#/Eb'],
+    "E Harmonic Minor": ['E', 'F#/Gb', 'G', 'A', 'B', 'C', 'E'],
+    "F Harmonic Minor": ['F', 'G', 'G#/Ab', 'A#/Bb', 'C', 'C#/Db', 'F'],
+    "F#/Gb Harmonic Minor": ['F#/Gb', 'G#/Ab', 'A', 'B', 'C#/Db', 'D', 'F#/Gb'],
+    "G Harmonic Minor": ['G', 'A', 'A#/Bb', 'C', 'D', 'D#/Eb', 'G'],
+    "Ab Melodic Minor": ['G#/Ab', 'A#/Bb', 'C', 'C#/Db', 'D#/Eb', 'F', 'G'],
+    "A Melodic Minor": ['A', 'B', 'C', 'D', 'E', 'F#/Gb', 'G#/Ab'],
+    "Bb Melodic Minor": ['A#/Bb', 'C', 'C#/Db', 'Eb', 'F', 'G', 'A'],
+    "B Melodic Minor": ['B', 'C#/Db', 'D', 'E', 'F#/Gb', 'G#/Ab', 'A#/Bb'],
+    "C Melodic Minor": ['C', 'D', 'D#/Eb', 'F', 'G', 'A', 'B'],
+    "C#/Db Melodic Minor": ['C#/Db', 'D#/Eb', 'E', 'F#/Gb', 'G#/Ab', 'B', 'C#/Db'],
+    "D Melodic Minor": ['D', 'E', 'F', 'G', 'A', 'A#/Bb', 'C#/Db'],
+    "Eb Melodic Minor": ['D#/Eb', 'F', 'F#/Gb', 'G#/Ab', 'A#/Bb', 'C', 'D'],
+    "E Melodic Minor": ['E', 'F#/Gb', 'G', 'A', 'B', 'C#/Db', 'D#/Eb'],
+    "F Melodic Minor": ['F', 'G', 'G#/Ab', 'A#/Bb', 'C', 'D', 'E'],
+    "F#/Gb Melodic Minor": ['F#/Gb', 'G#/Ab', 'A', 'B', 'C#/Db', 'D#/Eb', 'F'],
+    "G Melodic Minor": ['G', 'A', 'A#/Bb', 'C', 'D', 'E', 'F#/Gb'],
+    "C Dorian": ['C', 'D', 'D#/Eb', 'F', 'G', 'A', 'A#/Bb'],
+    "D Dorian": ['D', 'E', 'F', 'G', 'A', 'B', 'C'],
+    "E Dorian": ['E', 'F#/Gb', 'G', 'A', 'B', 'C#/Db', 'D'],
+    "F Dorian": ['F', 'G', 'G#/Ab', 'A#/Bb', 'C', 'D', 'D#/Eb'],
+    "G Dorian": ['G', 'A', 'A#/Bb', 'C', 'D', 'E', 'F'],
+    "A Dorian": ['A', 'B', 'C', 'D', 'E', 'F#/Gb', 'G'],
+    "B Dorian": ['B', 'C#/Db', 'D', 'E', 'F#/Gb', 'G#/Ab', 'A'],
+    "C Mixolydian": ['C', 'D', 'E', 'F', 'G', 'A', 'A#/Bb'],
+    "D Mixolydian": ['D', 'E', 'F#/Gb', 'G', 'A', 'B', 'C'],
+    "E Mixolydian": ['E', 'F#/Gb', 'G#/Ab', 'A', 'B', 'C#/Db', 'D'],
+    "F Mixolydian": ['F', 'G', 'A', 'A#/Bb', 'C', 'D', 'D#/Eb'],
+    "G Mixolydian": ['G', 'A', 'B', 'C', 'D', 'E', 'F'],
+    "A Mixolydian": ['A', 'B', 'C#/Db', 'D', 'E', 'F#/Gb', 'G'],
+    "B Mixolydian": ['B', 'C#/Db', 'D#/Eb', 'E', 'F#/Gb', 'G#/Ab', 'A'],
+    "C Phrygian": ['C', 'C#/Db', 'D#/Eb', 'F', 'G', 'G#/Ab', 'A#/Bb'],
+    "D Phrygian": ['D', 'D#/Eb', 'F', 'G', 'A', 'A#/Bb', 'C'],
+    "E Phrygian": ['E', 'F', 'G', 'A', 'B', 'C', 'D'],
+    "F Phrygian": ['F', 'F#/Gb', 'G#/Ab', 'A#/Bb', 'C', 'C#/Db', 'D#/Eb'],
+    "G Phrygian": ['G', 'G#/Ab', 'A#/Bb', 'C', 'D', 'D#/Eb', 'F'],
+    "A Phrygian": ['A', 'A#/Bb', 'B', 'C#/Db', 'D', 'D#/Eb', 'F'],
+    "B Phrygian": ['B', 'C', 'D', 'E', 'F#/Gb', 'G', 'A'],
 }
 
 heading_text = document.getElementById("heading");
@@ -76,6 +133,11 @@ function checkKeys() {
             x.innerHTML = key;
             x.style.marginLeft = "10px";
             x.style.marginRight = "10px";
+            x.style.marginBottom = "10px"
+            x.style.backgroundColor = "white";
+            x.style.color = "black";
+            x.style.padding = "5px";
+            x.style.border = "2px solid black";
             output.appendChild(x);
             //output.innerHTML = output.innerHTML + key + " ";
         }
